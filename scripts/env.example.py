9e58961399b414dd8fb7cb51ddf59055ebed02d0
groovehunter <groovehunter@posteo.org>
2026-06-18 23:34:10 +0200

    env.example

---
+++ scripts/env.example.py
@@ -0,0 +1,37 @@
+from collections import OrderedDict
+from pathlib import Path
+
+# Beispielwert, an das Zielprojekt anpassen.
+home_dir = Path.home()
+project_subpath = "dev_sub"
+target_env = "dev"
+basedir = home_dir / project_subpath / "k8s_weblogic"
+template_dir = basedir / "k8s"
+valuestore_dir = basedir
+outpath = basedir / "out"
+template_file_name = "deploy-wls-admin.yaml"
+
+template_defaults = {
+    "source": "project",
+    "transform": "fill_config_template",
+}
+template_files = [template_file_name]
+
+data_file_defaults = {
+    "source": "project",
+    "transform": "fill_simple_template",
+    "env": "fallback",
+}
+data_files = OrderedDict(
+    [
+        (
+            "resources",
+            {
+                "source": "project",
+                "transform": "fill_simple_template",
+                "env": "no",
+            },
+        ),
+    ]
+)
+
